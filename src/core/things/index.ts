4ea18088cfcd6c02bbf82f503766bf65d9246603
Xanonymous <trusaidlin@gmail.com>
2021-03-05 18:32:56 +0800

    refactor ting class member names

--- src/core/things/index.ts
+++ src/core/things/index.ts
@@ -1,5 +1,4 @@
 import * as PIXI from 'pixi.js'
-import {getBlockSize} from "@/core/utils/screen";
 import {Towards} from "@/core/types/things";
 
 export abstract class Thing extends PIXI.Sprite {
@@ -8,23 +7,33 @@ export abstract class Thing extends PIXI.Sprite {
   private _maxBlockX?: number
   private _maxBlockY?: number
   private _defaultTowards?: Towards
+  private _blockSize?: number
   
   protected constructor(
     texture: PIXI.Texture,
     defaultBlockX: number,
     defaultBlockY: number,
+    blockSize: number,
     maxBlockX: number,
     maxBlockY: number,
     defaultTowards?: Towards,
   ) {
-    super(texture);
+    // provide the texture to the sprite.
+    super(texture)
+    
+    // setup default positions.
     this._blockX = defaultBlockX
     this._blockY = defaultBlockY
+    
+    // setup maximum block point on map.
     this._maxBlockX = maxBlockX
     this._maxBlockY = maxBlockY
+    
+    // setup default sprite towards.
     this._defaultTowards = defaultTowards;
     
-    const blockSize = getBlockSize()
+    // setup block size.
+    this._blockSize = blockSize
     this.height = blockSize
     this.width = blockSize
   }
