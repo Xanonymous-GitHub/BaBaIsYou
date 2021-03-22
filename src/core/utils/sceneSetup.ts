5699c98821d259e37895d7b7a781581c0690e4e8
Ricky Hu <ricky9667@gmail.com>
2021-03-22 15:00:15 +0800

    Add types to scene setup and rename variables

--- src/core/utils/sceneSetup.ts
+++ src/core/utils/sceneSetup.ts
@@ -2,37 +2,48 @@ import {SceneSetup} from '../types';
 import {Species} from '../resource';
 import {ThingSetup} from '../types/things';
 
+export interface SceneSetupImport {
+    readonly id: string
+    readonly name: string
+    readonly thingsMap: Array<ThingsMapImport>
+}
 
-const convertSceneToObject = (json): SceneSetup => {
+export interface ThingsMapImport {
+    readonly species: Species
+    readonly name: string
+    readonly thingsSetup: Array<ThingSetup>
+}
+
+const convertSceneToObject = (sceneSetupImport: SceneSetupImport): SceneSetup => {
     const thingsMap = new Map<{ species: Species, name: string }, Array<ThingSetup>>()
-    for (const thing of json['thingsMap']) {
-        const key = {species: thing['species'], name: thing['name']}
-        thingsMap.set(key, thing['thingSetup'])
+    for (const thing of sceneSetupImport.thingsMap) {
+        const key = {species: thing.species, name: thing.name}
+        thingsMap.set(key, thing.thingsSetup)
     }
 
     return {
-        id: json['id'],
-        name: json['name'],
+        id: sceneSetupImport.id,
+        name: sceneSetupImport.name,
         thingsMap
     }
 }
 
-const getSceneJsonFromLocal = async (path: string) => {
-    return await new Promise(function (resolve, reject) {
+const loadSceneSetupImport = async (filePath: string): Promise<SceneSetupImport> => {
+    return await new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
-        xhr.onload = function () {
+        xhr.onload = () => {
             if (xhr.status === 200) {
                 resolve(JSON.parse(xhr.responseText))
             } else {
                 reject('Error occurred while loading JSON from local')
             }
         }
-        xhr.open('GET', path, true)
+        xhr.open('GET', filePath, true)
         xhr.send()
     })
 }
 
-export const getSceneSetup = async (path: string): Promise<SceneSetup> => {
-    const result = await getSceneJsonFromLocal(path)
-    return convertSceneToObject(result)
+export const getSceneSetup = async (filePath: string): Promise<SceneSetup> => {
+    const sceneSetupImport = await loadSceneSetupImport(filePath)
+    return convertSceneToObject(sceneSetupImport)
 }
