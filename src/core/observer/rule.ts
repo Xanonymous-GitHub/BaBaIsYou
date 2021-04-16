89b5a135493e55c71650166ef4781bf57df8930c
Ricky Hu <ricky9667@gmail.com>
2021-04-16 12:00:44 +0800

    Add feature interfaces and convert methods in rules to features

--- src/core/observer/rule.ts
+++ src/core/observer/rule.ts
@@ -5,49 +5,114 @@ import {OperatorType} from '../types/operators';
 import {ThingType} from '../types';
 import {CharacterType} from '../types/characters';
 
+export interface FeatureCondition {
+    feature: NounType | PropertyType
+    on: Array<NounType>
+    near: Array<NounType>
+    facing: Array<NounType>
+}
+
+export interface FeatureList {
+    _is: Array<FeatureCondition>
+    _has: Array<FeatureCondition>
+    _make: Array<FeatureCondition>
+}
+
 export interface RuleController {
-    $is: (thing: Thing, requestedProperty: PropertyType) => boolean
-    addProperty: (property: PropertyType, thing: Thing) => void
-    removeProperty: (property: PropertyType, thing: Thing) => void
+    $is: (requester: Thing, requestedFeature: PropertyType) => boolean
+    // $has: (requester: Thing, requestedFeature: NounType) => boolean
+    // $make: (requester: Thing, requestedFeature: NounType) => boolean
+    addFeature: (thing: Thing, operator: OperatorType, featureCondition: FeatureCondition) => void
+    removeFeature: (thing: Thing, operator: OperatorType, featureCondition: FeatureCondition) => void
 }
 
 
 class RuleControllerConcrete implements RuleController {
-    private _ruleMap: Map<ThingType, Array<PropertyType>>
+    private _ruleMap: Map<ThingType, Array<PropertyType>> // will be deprecated
+    private _featureMap: Map<ThingType, FeatureList>
 
     constructor() {
-        this._ruleMap = new Map();
+        this._ruleMap = new Map(); // will be deprecated
+        this._featureMap = new Map();
 
         // set default property
+        // should be changed to save in FeatureList
         (Object.keys(NounType) as Array<NounType>).map(noun => this._ruleMap.set(noun, [PropertyType.PUSH]));
         (Object.keys(PropertyType) as Array<PropertyType>).map(property => this._ruleMap.set(property, [PropertyType.PUSH]));
         (Object.keys(OperatorType) as Array<OperatorType>).map(operator => this._ruleMap.set(operator, [PropertyType.PUSH]));
 
         // DEBUG
-        this._ruleMap.set(CharacterType.BABA, [PropertyType.YOU])
+        this._ruleMap.set(CharacterType.BABA, [PropertyType.YOU]) // will be deprecated
+
+        const featureConditions = {feature: PropertyType.YOU, on: [], near: [], facing: []}
+        const featureList = {_is: [featureConditions], _has: [], _make: []}
+        this._featureMap.set(CharacterType.BABA, featureList)
     }
 
-    public $is(thing: Thing, requestedProperty: PropertyType): boolean {
-        const noun = thing.name as NounType
-        const properties = this._ruleMap.get(noun)
-        return Boolean(properties && properties.includes(requestedProperty))
-    }
+    public $is(requester: Thing, requestedFeature: PropertyType): boolean {
+        const requesterName = requester.name as NounType
+        const featureConditions = this._featureMap.get(requesterName) as FeatureList
 
-    public addProperty(property: PropertyType, thing: Thing): void {
-        const thingType = thing.name as ThingType
-        if (!this._ruleMap.has(thingType)) {
-            this._ruleMap.set(thingType, [property])
-            return
+        let containsProperty = false
+        for (const featureCondition of featureConditions._is) {
+            if (featureCondition.feature === requestedFeature) {
+                containsProperty = true
+                break
+            }
         }
-        this._ruleMap.get(thingType)!.push(property)
+
+        // NOUN IS NOUN should be processed
+
+        return Boolean(featureConditions && containsProperty)
     }
 
-    public removeProperty(property: PropertyType, thing: Thing): void {
+    public addFeature(thing: Thing, operator: OperatorType, featureCondition: FeatureCondition): void {
         const thingType = thing.name as ThingType
-        if (!this._ruleMap.has(thingType)) throw new Error(`thingType ${thingType} not been recorded yet`)
-        const indexToRemove = this._ruleMap.get(thingType)!.indexOf(property)
-        if (indexToRemove === -1) throw new Error(`thingType ${thingType} does not have property ${property}`)
-        this._ruleMap.get(thingType)!.splice(indexToRemove, 1)
+
+        if (!this._featureMap.has(thingType)) {
+            const featureList = {_is: [], _has: [], _make: []} as FeatureList
+            this._featureMap.set(thingType, featureList)
+        }
+
+        switch (operator) {
+            case OperatorType.IS:
+                this._featureMap.get(thingType)!._is.push(featureCondition)
+                break
+            case OperatorType.HAS:
+                this._featureMap.get(thingType)!._has.push(featureCondition)
+                break
+            case OperatorType.MAKE:
+                this._featureMap.get(thingType)!._make.push(featureCondition)
+                break
+            default:
+                throw new Error(`operator ${operator} should be a verb (OperatorType.IS, OperatorType.HAS, OperatorType.MAKE) instead of an adjective`)
+        }
+    }
+
+    public removeFeature(thing: Thing, operator: OperatorType, featureCondition: FeatureCondition): void {
+        const thingType = thing.name as ThingType
+
+        if (!this._featureMap.has(thingType)) throw new Error(`thingType ${thingType} not been recorded yet`)
+        let removeIndex = -1
+        switch (operator) {
+            case OperatorType.IS:
+                removeIndex = this._featureMap.get(thingType)!._is.indexOf(featureCondition)
+                if (removeIndex === -1) throw new Error(`thingType ${thingType} does not contain feature ${featureCondition}`)
+                this._featureMap.get(thingType)!._is.splice(removeIndex, 1)
+                break
+            case OperatorType.HAS:
+                removeIndex = this._featureMap.get(thingType)!._has.indexOf(featureCondition)
+                if (removeIndex === -1) throw new Error(`thingType ${thingType} does not contain feature ${featureCondition}`)
+                this._featureMap.get(thingType)!._has.splice(removeIndex, 1)
+                break
+            case OperatorType.MAKE:
+                removeIndex = this._featureMap.get(thingType)!._make.indexOf(featureCondition)
+                if (removeIndex === -1) throw new Error(`thingType ${thingType} does not contain feature ${featureCondition}`)
+                this._featureMap.get(thingType)!._make.splice(removeIndex, 1)
+                break
+            default:
+                throw new Error(`operator ${operator} should be a verb (OperatorType.IS, OperatorType.HAS, OperatorType.MAKE) instead of an adjective`)
+        }
     }
 }
 
