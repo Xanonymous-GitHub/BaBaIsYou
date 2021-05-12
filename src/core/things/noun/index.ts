c57e0f8136484af9d5db8915c7e231bf51ede46b
Ricky Hu <ricky9667@gmail.com>
2021-05-12 15:36:40 +0800

    Create noun thing classes for new levels

--- src/core/things/noun/index.ts
+++ src/core/things/noun/index.ts
@@ -2,11 +2,19 @@ import {ThingCreator} from '../factory';
 import {createTextBaba} from './textBaba';
 import {createTextWall} from './textWall';
 import {createTextFlag} from './textFlag';
+import {createTextCrab} from './textCrab';
+import {createTextSkull} from './textSkull';
+import {createTextStar} from './textStar';
+import {createTextRock} from './textRock';
 
 const nouns: { [index: string]: ThingCreator } = {
     TextBaba: createTextBaba,
     TextWall: createTextWall,
-    TextFlag: createTextFlag
+    TextFlag: createTextFlag,
+    TextCrab: createTextCrab,
+    TextSkull: createTextSkull,
+    TextStar: createTextStar,
+    TextRock: createTextRock
 }
 
 export default nouns
