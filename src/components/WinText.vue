8f21a1f8daa957a81f80d743fe7d5b48e129b566
Ricky Hu <ricky9667@gmail.com>
2021-06-21 15:18:35 +0800

    Change wintext to congratulations text

--- src/components/WinText.vue
+++ src/components/WinText.vue
@@ -1,7 +1,8 @@
 <template>
-  <img :src='winPictureUrl' alt=''>
+  <img :src='congratulationsUrl' alt='Congratulations' class='w-3/5'>
 </template>
 
 <script setup lang='ts'>
   import winPictureUrl from '@/assets/images/win.gif'
+  import congratulationsUrl from '@/assets/images/congratulations.png'
 </script>
