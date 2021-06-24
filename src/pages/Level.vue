7ad560189b6018408e60b5828fab2090f9775f47
Ricky Hu <ricky9667@gmail.com>
2021-06-25 06:01:28 +0800

    Adjust level page columns

--- src/pages/Level.vue
+++ src/pages/Level.vue
@@ -8,7 +8,7 @@
         bg-gray-900'>
     <div class='card-container w-4/5 relative mx-auto my-auto'>
       <button v-for='(level, i) of levels' :key='i' type='button'
-              class='w-25 h-10 py-1 px-2 sm:(py-2 px-4) flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg'
+              class='w-60 h-10 py-1 px-2 sm:(py-2 px-4) flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg'
               @click.stop.prevent='startGame(level)'>
         {{ level.name }}
       </button>
@@ -39,7 +39,7 @@
 <style lang='scss' scoped>
   .card-container {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(min(100%, 6.5rem), 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(min(100%, 15rem), 1fr));
     grid-template-rows: auto;
     justify-items: center;
     gap: 1.5rem;
