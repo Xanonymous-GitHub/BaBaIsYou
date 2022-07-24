44ed6698ff4878b1071df8f2acd3fc2924186b50
Xanonymous <trusaidlin@gmail.com>
2022-07-25 00:23:35 +0800

    chore: upgrade packages - vite 3

--- vite.config.ts
+++ vite.config.ts
@@ -20,22 +20,7 @@ export default defineConfig({
     createHtmlPlugin()
   ],
   build: {
-    minify: 'terser',
-    brotliSize: true,
     cssCodeSplit: true,
-    terserOptions: {
-      sourceMap: false,
-      ecma: 5,
-      ie8: true,
-      safari10: true,
-      keep_classnames: false,
-      keep_fnames: false,
-      compress: true,
-      format: {
-        preserve_annotations: false,
-        comments: false
-      }
-    },
     chunkSizeWarningLimit: 100000,
     rollupOptions: {
       output: {
