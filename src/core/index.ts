574ccbf4b6a755fe6e75fd25fa7e0c459ec490f6
Xanonymous <trusaidlin@gmail.com>
2024-11-17 02:50:13 +0800

    refactor: 💡 re-expose game core module for new pixi api

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,28 +5,36 @@ import { createContainerBuilder } from './builders/container'
 import { createSpriteBuilder } from './builders/sprite'
 import { startLevel, setGameOverOutsideHandler, setYouGoneOutsideHandler, pause, resume } from '@/core/game'
 import type { GameCore } from '@/core/types'
+import { appOptions } from './app/configs'
 
 const app = createGameApp()
+
 export const store = createGameStore()
 
-store.bindAppToScreenService(app)
+const gameInit = async () => {
+  await app.init(appOptions)
+  store.bindAppToScreenService(app)
 
-const spriteController = createSpriteBuilder()
-store.setSpriteBuilder(spriteController)
+  const spriteController = createSpriteBuilder()
+  store.setSpriteBuilder(spriteController)
 
-const containerController = createContainerBuilder()
-store.setContainerBuilder(containerController)
+  const containerController = createContainerBuilder()
+  store.setContainerBuilder(containerController)
 
-const stageController = createStageBuilder(app.stage)
-store.setStageBuilder(stageController)
-
-const gameCore: GameCore = {
-  gameView: app.view,
-  startLevel,
-  pause,
-  resume,
-  setYouGoneOutsideHandler,
-  setGameOverOutsideHandler
+  const stageController = createStageBuilder(app.stage)
+  store.setStageBuilder(stageController)
 }
 
-export default new Promise<GameCore>($export => $export(gameCore))
+const gameCore = async (): Promise<GameCore> => {
+  await gameInit()
+  return {
+    gameView: app.canvas,
+    startLevel,
+    pause,
+    resume,
+    setYouGoneOutsideHandler,
+    setGameOverOutsideHandler
+  }
+}
+
+export default new Promise<Promise<GameCore>>(($export) => $export(gameCore()))
